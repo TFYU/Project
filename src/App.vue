d7dd82c71b10c0d20deb1befdf28ee8ba42eb731
TFYU <842567977@qq.com>
2022-08-23 18:00:03 +0800

    third

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
 <div class='app'>
   <h1>赛诚app</h1>
+  <!-- iconfint使用 -->
+  <!-- 方法一：font-class -->
+  <i class="iconfont sc-mima"></i>
+  <!-- 方法二：编码方式 -->
+  <i class="iconfont" style="color:pink">&#xe626;</i>
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="info">信息按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+  <van-tabs v-model="active">
+  <van-tab title="标签 1">内容 1</van-tab>
+  <van-tab title="标签 2">内容 2</van-tab>
+  <van-tab title="标签 3">内容 3</van-tab>
+  <van-tab title="标签 4">内容 4</van-tab>
+  </van-tabs>
 </div>
 </template>
 
@@ -11,13 +27,13 @@ export default {
   components: {},
   data () {
     return {
-
+       active: 0,
     }
   },
   computed: {},
   watch: {},
   methods: {
-
+   
   },
   created () {
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//引入vant样式
+import { Button, Tab, Tabs } from 'vant';
+//引入组件样式
+import 'vant/lib/index.less'
+Vue.use(Button);
+Vue.use(Tab);
+Vue.use(Tabs);
+
 //引入全局样式
 import '@/styles/index.less'
+
 
 Vue.config.productionTip = false
 
